b6559c313e7c4a2d1035e542f8ad471834f2182e
Nova-Foster <Novafoster3232@gmail.com>
2026-01-02 22:10:44 +0000

    Untracked config files

---
+++ rust_projects/chapter-3/variables-and-mutability/variables/target/debug/deps/variables-3ee7cae4ff3acb1c.d
@@ -0,0 +1,5 @@
+C:\Users\alexf\self-taught_rust\rust_projects\chapter-3\variables-and-mutability\variables\target\debug\deps\variables-3ee7cae4ff3acb1c.d: src\main.rs
+
+C:\Users\alexf\self-taught_rust\rust_projects\chapter-3\variables-and-mutability\variables\target\debug\deps\libvariables-3ee7cae4ff3acb1c.rmeta: src\main.rs
+
+src\main.rs:
